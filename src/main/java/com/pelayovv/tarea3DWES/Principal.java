e8bb42f3837ff009258043d85699ccadeb0e7e55
pelayovv20 <pelayovv20educastur.es>
2024-11-21 14:12:01 +0100

    Creada clase Principal y arrancando el programa

---
+++ src/main/java/com/pelayovv/tarea3DWES/Principal.java
@@ -0,0 +1,22 @@
+package com.pelayovv.tarea3DWES;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class Principal implements CommandLineRunner {
+	
+	//@Autowired
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("INI");
+		
+		System.out.println("-------------------");
+		
+		
+		System.out.println("FIN");
+		
+	}
+
+}
--- src/main/java/com/pelayovv/tarea3DWES/Tarea3DwesApplication.java
+++ src/main/java/com/pelayovv/tarea3DWES/Tarea3DwesApplication.java
@@ -2,9 +2,15 @@ package com.pelayovv.tarea3DWES;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Tarea3DwesApplication {
+	
+	@Bean
+	public Principal applicationStartupRunner() {
+		return new Principal();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tarea3DwesApplication.class, args);
