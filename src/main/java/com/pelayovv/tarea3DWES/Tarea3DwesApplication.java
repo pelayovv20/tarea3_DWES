4eb05182e7356ec975944b9a615ce3b6deaa51d0
pelayovv20 <pelayovv20educastur.es>
2024-11-21 12:45:34 +0100

    Commit inicial

---
+++ src/main/java/com/pelayovv/tarea3DWES/Tarea3DwesApplication.java
@@ -0,0 +1,13 @@
+package com.pelayovv.tarea3DWES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3DwesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea3DwesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelayovv/tarea3DWES/Tarea3DwesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelayovv.tarea3DWES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3DwesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
